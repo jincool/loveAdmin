f006e4a87073b69fae235b85fdef2ea3d942a9ec
jincool <634596685@qq.com>
2019-05-02 00:12:28 +0800

    Update Home.php

--- application/index/controller/Home.php
+++ application/index/controller/Home.php
@@ -10,6 +10,7 @@ namespace app\index\controller;
 
 
 use app\common\controller\Base;
+use think\File;
 use think\facade\Request;
 
 class Home extends Base
@@ -17,7 +18,20 @@ class Home extends Base
 
     public function upLoadImg(){
 
-        $file = request()->file('file');
-        return dump($file->getInfo());
+        //获取上传图片信息
+        $file=Request::file('file');
+        if ($file) {
+            //文件信息验证，成功上传到指定目录,以public为起始
+            $info=$file ->validate([
+                'size' => 1000000,
+//                'ext' =>'jpeg,jpg,png,gif' ,
+            ])->move('uploads/');
+            if ($info) {
+                // 获取服务器中文件的名字并赋予表单信息title_img
+                echo $info ->getSaveName();
+            } else {
+                $this->error($file ->getError());
+            }
+        }
     }
 }
