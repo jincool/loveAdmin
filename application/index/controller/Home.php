c271ce15c99df0819c735806e55e06a58e593116
jincool <634596685@qq.com>
2019-05-05 09:28:31 +0800

    Update Home.php

--- application/index/controller/Home.php
+++ application/index/controller/Home.php
@@ -10,14 +10,25 @@ namespace app\index\controller;
 
 
 use app\common\controller\Base;
-use think\facade\Request;
-
 class Home extends Base
 {
 
-    public function upLoadImg(){
+    public function upLoadImg()
+    {
 
+        //获取表单上传文件
         $file = request()->file('file');
-        return dump($file->getInfo());
+        if (empty($file)) {
+            $this->error('请选择上传文件');
+        }
+        //移动到框架应用根目录/public/uploads/ 目录下
+        $info = $file->move( '../uploads');
+        if ($info) {
+//            $this->success('文件上传成功');
+         echo $info->getFilename();
+        } else {
+            //上传失败获取错误信息
+            $this->error($file->getError());
+        }
     }
 }
