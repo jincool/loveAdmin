2f63c5bf2f4b60c8f40fe6146b25a00a6fa99bb9
jincool <634596685@qq.com>
2019-05-09 10:53:18 +0800

    新增获取纪念日背景图片

--- application/index/controller/Home.php
+++ application/index/controller/Home.php
@@ -12,6 +12,7 @@ namespace app\index\controller;
 use app\common\controller\Base;
 use think\File;
 use think\facade\Request;
+use app\common\model\Setting;
 class Home extends Base
 {
 
@@ -20,6 +21,7 @@ class Home extends Base
 
         //获取上传图片信息
         $file=Request::file('file');
+        $wid=Request::param('wid');
         if ($file) {
             //文件信息验证，成功上传到指定目录,以public为起始
             $info=$file ->validate([
@@ -28,10 +30,35 @@ class Home extends Base
             ])->move('uploads/');
             if ($info) {
                 // 获取服务器中文件的名字
-                echo $info ->getSaveName();
+                $url = $info ->getSaveName();
+                $user=Setting::where('wid',$wid)->find();
+                if (!empty($user)){
+                    $user->home_bg=$url;
+                    $user->save();
+                }else{
+                    $setting = new Setting;//实例化Setting表模型
+                    $setting->wid = $wid;
+                    $setting->home_bg = $url;
+                    $setting->save();
+                }
+
+
             } else {
                 $this->error($file ->getError());
             }
         }
     }
+
+    public function bgImg(){
+        if (!empty(Request::param('wid'))){
+            $wid=Request::param('wid');
+            $info=Setting::where('wid',$wid)->find();
+            $img=$info->home_bg;
+           echo $img;
+        }
+
+    }
+
+
+
 }
